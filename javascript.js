93735be46a537c7b733a1c8d1026e7c4a3f010ed
Vivek Raj <114300476+VivekRaaZ@users.noreply.github.com>
2023-08-03 02:45:29 +0530

    Add Update javascript.js file

--- javascript.js
+++ javascript.js
@@ -1,10 +1,12 @@
 const Menu = document.getElementById('burger');
 const closeMenu = document.querySelector('.close-icon');
 const openMenu = document.querySelector('#navbar1');
+const listbtn1 = document.querySelector('#list33');
+const listbtn2 = document.querySelector('#list34');
+const listbtn3 = document.querySelector('#list35');
 
 const showNavbar = () => {
   Menu.style.display = 'block';
-  Menu.classList.toggele('#navbar1');
 };
 
 const hideNavbar = () => {
@@ -13,3 +15,6 @@ const hideNavbar = () => {
 
 closeMenu.addEventListener('click', hideNavbar);
 openMenu.addEventListener('click', showNavbar);
+listbtn1.addEventListener('click', hideNavbar);
+listbtn2.addEventListener('click', hideNavbar);
+listbtn3.addEventListener('click', hideNavbar);
